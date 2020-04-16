ab4f803d9e551c097c1fed5d34f88db6e1e1b8c3
valntinaf <feruereru@gmail.com>
2020-04-16 11:17:45 -0500

    Adding icons

--- labule.py
+++ labule.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -19,7 +21,6 @@ class Dialog(QDialog):
         self.setStyleSheet("background-color: #020202")
         layout = QVBoxLayout()
 
-
         label_time = QLabel(self)
         label_time.setText(str(currentDT.hour)+":"+str(currentDT.minute))
         label_time.setFont(QFont('Pontano Sans', 40))
@@ -32,13 +33,20 @@ class Dialog(QDialog):
         label_date.setFont(QFont('Pontano Sans', 30))
         label_date.setAlignment(Qt.AlignCenter)
         label_date.setStyleSheet("color: #efefef")
-        label_date.top = 10
         layout.addWidget(label_date)
 
         wire = Wireless()
         connected_ESSID = wire.current()
+
+        grid = QGridLayout()
+        label_network_icon = QLabel(self)
+        temp = QPixmap('icons/wifi.svg')
+        pixmap = temp.scaled(32, 32, QtCore.Qt.KeepAspectRatio)
+        label_network_icon.setPixmap(pixmap)
+        layout.addWidget(label_network_icon)
+
         label_connected_ESSID = QLabel(self)
-        label_connected_ESSID.setText("Connected:\t\t"+connected_ESSID)
+        label_connected_ESSID.setText(connected_ESSID)
         label_connected_ESSID.setFont(QFont('Pontano Sans', 20))
         label_connected_ESSID.setAlignment(Qt.AlignCenter)
         label_connected_ESSID.setStyleSheet("color: #efefef")
@@ -46,10 +54,13 @@ class Dialog(QDialog):
         layout.addWidget(label_connected_ESSID)
 
         m = Mixer()
-        mute = m.getmute()[0]
+        muted = m.getmute()[0]
         vol = m.getvolume()[0]
         label_volume = QLabel(self)
-        label_volume.setText("Volume:\t\t"+str(vol))
+        if muted:
+            label_volume.setText("Volume:\t\t"+"muted")
+        else:
+            label_volume.setText("Volume:\t\t"+str(vol))
         label_volume.setFont(QFont('Pontano Sans', 20))
         label_volume.setAlignment(Qt.AlignCenter)
         label_volume.setStyleSheet("color: #efefef")
@@ -65,5 +76,4 @@ if __name__ == "__main__":
     app = QApplication([])
     dialog = Dialog()
     dialog.show()
-    print(dir(dialog))
     app.exec_()
